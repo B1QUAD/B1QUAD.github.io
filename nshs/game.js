3618bcc3c0e56b4fd4e2de4144d45ef7896bc46f
B1QUAD <accntmail.745@gmail.com>
2021-01-15 21:36:40 -0600

    Game beta launched

---
+++ nshs/game.js
@@ -0,0 +1,48 @@
+var gameList = [
+    {
+        "name": "Online Uno | Uno Freak",
+        "description": "Online Uno game hosted by Uno Freak.",
+        "link": "https://play.unofreak.com/"
+    },
+    {
+        "name": "Lotería",
+        "description": "The game of Lotería is commemorated in this google doodle",
+        "link": "https://www.google.com/doodles/celebrating-loteria"
+    },
+    {
+        "name": "Scattergories | Scattergoriesonline.net",
+        "description": "An online scattegories game in Spanish",
+        "link" : "https://scattergoriesonline.net/new-game.xhtml#lang=es"
+    },
+    {
+        "name": "Online BattleShip",
+        "description": "An online game of BattleShip to play with friends.",
+        "link": "https://www.battleshiponline.org/"
+    },
+    {
+        "name": "",
+        "description": "",
+        "link": ""
+    }
+]
+
+function getGame() {
+
+    return gameList[Math.floor(Math.random() * gameList.length)];
+}
+
+function set_text() {
+    let log = document.getElementById("log");
+    log.innerHTML = jsToHTML();
+}
+
+function jsToHTML() {
+
+    game = getGame();
+    var out;
+    var i;
+    out = '<p>' + game.name + '</p> <p>' + game.description + '</p>';
+    out += '<p><a href=' + game.link + ' target="_blank" rel="noopener noreferrer">link</a></p>';
+    // console.log(out);
+    return out;
+}
